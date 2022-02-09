7651f221708019bcbce125bc6f47da7216d8f036
William <work@suppo.fr>
2022-02-09 18:04:40 +0100

    On automatise avec l'import de mes articles.

---
+++ feed.php
@@ -0,0 +1,45 @@
+<?php
+
+$posts = fetchPosts('https://laravel-france.com/rss?author=William');
+
+$updatedReadme = feedReadme($posts);
+
+writeReadme($updatedReadme);
+
+function fetchPosts(string $url)
+{
+    $xml = file_get_contents($url);
+
+    $dom = new DOMDocument();
+
+    $dom->loadXML($xml);
+
+    $items = $dom->getElementsByTagName('item');
+
+    $posts = [];
+
+    foreach ($items as $item) {
+        $posts[] = [
+            'title' => $item->getElementsByTagName('title')->item(0)->nodeValue,
+            'link' => $item->getElementsByTagName('link')->item(0)->nodeValue,
+        ];
+    }
+
+    return $posts;
+}
+
+function feedReadme(array $posts)
+{
+    $stub = file_get_contents('README.stub');
+
+    $replace = array_map(function ($post) {
+        return sprintf('+ [%s](%s)', $post['title'], $post['link']);
+    }, $posts);
+
+    return str_replace('{posts}', implode("\n", $replace), $stub);
+}
+
+function writeReadme(string $content)
+{
+    file_put_contents('README.md', $content);
+}
